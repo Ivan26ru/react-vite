685555212ef18f17a4eeff3028f568493cd4c978
Ivan <it-1ivan@ya.ru>
2024-03-03 13:33:52 +0500

    add useEffect, useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 export function App() {
 	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		console.log("first init");
+	}, [count]);
+
+	const btn = useRef();
 
 	function handleClick() {
 		setCount(count + 1);
 	}
 
 	return (
-		<button onClick={handleClick}>
+		<button onClick={handleClick} ref={btn}>
 			You pressed me {count} times
 		</button>
 	);
