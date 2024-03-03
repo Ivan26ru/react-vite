1df0b6c282fd83cf56ca9b4c1dd1ea92fe70926b
Ivan <it-1ivan@ya.ru>
2024-03-03 21:54:05 +0500

    use typescript

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import List from "./components/List.tsx";
+import Button from "./components/Button.tsx";
+
+const list = [{
+    title: "title 123",
+    name: "name asdfas;dlfkhja;sdkfj",
+}, {
+    title: "title 2",
+    name: "name 2",
+}];
+
+function App() {
+    return (
+        <div>
+            APP
+            <ul>
+                {list.map((
+                    {name, title},
+                    index) =>
+                    <List key={index} name={name} title={title}/>
+                )}
+            </ul>
+
+            <Button type={{color: 'red'}}>Название любое</Button>
+        </div>)
+}
+
+export default App
