a3a364c93c893d069b8c961bd1fa36737bad35fe
Ivan <it-1ivan@ya.ru>
2024-03-10 13:05:16 +0500

    Generics 3.5

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,58 @@ function App() {
     // const bool = true;
     // const fn = () => {};
 
+    const myArray: MyArray<number> = [1, 2, 3];
+
+    interface MyArray<T> {
+        [N: number]: T
+
+    //     map<U>(fn: (el: T, index: number, arr: MyArray<T>)=>U):MyArray<U>
+    }
+    //
+    // myArray.map()
+    //
+    // function identity<T>(arg: T): T {
+    //     return arg;
+    // }
+    //
+    // let result = identity(12);
+    //
+    // function getLen <T extends Array<any>>(arr: T):number{
+    //     return arr.length;
+    // }
+    //
+    // interface IValueWithType<T> {
+    //     type: string;
+    //     value: T
+    // }
+    //
+    // function withType<U>(arg: U) : IValueWithType<U>{
+    //     return {
+    //         type: typeof arg,
+    //         value: arg
+    //     }
+    // }
+    //
+    // const result2 = withType(123);
+
+
+    interface IExample<T> {
+        type: string;
+        value: T;
+        isEmpty: boolean;
+    }
+
+    const omittedObject: Omit<IExample<string>, 'isEmpty' | 'value'> = {
+
+    }
+
+    const picked: Pick<IExample<number>, 'isEmpty'> ={
+
+    }
+
+    const partical: Partial<IExample<object>> = {
+
+    }
 
     return (
         <div>
