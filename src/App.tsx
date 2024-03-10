031e46ba5b8a811a42b031c74bd3647a11fac905
Ivan <it-1ivan@ya.ru>
2024-03-10 13:21:03 +0500

    commit old code

--- src/App.tsx
+++ src/App.tsx
@@ -15,69 +15,9 @@ const list = [{
 ];
 
 function App() {
-    // const str = 'string';
-    // const num = 2;
-    // const obj = {};
-    // const arr = [];
-    // const null = null;
-    // const sym = Symbol();
-    // const und = undefined;
-    // const _void = void 0;
-    // const bool = true;
-    // const fn = () => {};
-
-    const myArray: MyArray<number> = [1, 2, 3];
-
-    interface MyArray<T> {
-        [N: number]: T
-
-    //     map<U>(fn: (el: T, index: number, arr: MyArray<T>)=>U):MyArray<U>
-    }
-    //
-    // myArray.map()
-    //
-    // function identity<T>(arg: T): T {
-    //     return arg;
-    // }
-    //
-    // let result = identity(12);
-    //
-    // function getLen <T extends Array<any>>(arr: T):number{
-    //     return arr.length;
-    // }
-    //
-    // interface IValueWithType<T> {
-    //     type: string;
-    //     value: T
-    // }
-    //
-    // function withType<U>(arg: U) : IValueWithType<U>{
-    //     return {
-    //         type: typeof arg,
-    //         value: arg
-    //     }
-    // }
-    //
-    // const result2 = withType(123);
 
 
-    interface IExample<T> {
-        type: string;
-        value: T;
-        isEmpty: boolean;
-    }
 
-    const omittedObject: Omit<IExample<string>, 'isEmpty' | 'value'> = {
-
-    }
-
-    const picked: Pick<IExample<number>, 'isEmpty'> ={
-
-    }
-
-    const partical: Partial<IExample<object>> = {
-
-    }
 
     return (
         <div>
