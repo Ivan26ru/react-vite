4b9883de77edec64d003e918267d2f2b351a85f1
Ivan <it-1ivan@ya.ru>
2024-03-13 22:36:34 +0500

    2.4 add template, use lists is array items

--- src/TermCard.tsx
+++ src/TermCard.tsx
@@ -4,7 +4,7 @@ export const TermCard = ({title, description}) => {
     return (
         <div className="term-card">
             <h2 className="term-card__title">{title}</h2>
-            <p className="term-card__description">{description}</p>
+            { description && <p className="term-card__description">{description}</p>}
         </div>
     );
 }
--- src/TermList.tsx
+++ src/TermList.tsx
@@ -1,20 +1,25 @@
 import "./TermList.scss";
 import {TermCard} from "./TermCard.tsx";
 
+const terms = [
+    {
+        title: "title1",
+        description: "desc1",
+    },
+    {
+        title: "title2",
+        description: "desc2"
+    },
+    {
+        title: "title3",
+    }
+];
 export const TermList = () => {
-    return <ul className="term-list">
-        <li>
-            <TermCard
-                title="React"
-                description="Библиотека для создания пользотельских интерфейсов"
-            />
-        </li>
-
-        <li>
-            <TermCard
-                title="React2"
-                description="Би2блиотека для создания пользотельских интерфейсов"
-            />
-        </li>
-    </ul>
+    return (<ul className="term-list">
+        {terms.map((item) => (
+            <li>
+                <TermCard title={item.title} description={item.description}/>
+            </li>
+        ))}
+    </ul>)
 }
