097add3c8578912115daa13007fed497a38afca7
Ivan <it-1ivan@ya.ru>
2024-03-13 23:19:27 +0500

    2.6 add key for terms.maps

--- src/TermList.tsx
+++ src/TermList.tsx
@@ -4,7 +4,7 @@ import {TermCard} from "./TermCard.tsx";
 export const TermList = ({terms}) => {
     return (<ul className="term-list">
         {terms.map((item) => (
-            <li>
+            <li key={item.id} className="term-list__item">
                 <TermCard title={item.title} description={item.description}/>
             </li>
         ))}
