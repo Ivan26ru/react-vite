893bb1ac9a340661aa9b6996f0f02a578ef9af28
Ivan <it-1ivan@ya.ru>
2024-03-18 22:35:42 +0500

    module-3 search component

--- module-3/src/App.jsx
+++ module-3/src/App.jsx
@@ -1,11 +1,23 @@
-import {useState} from 'react'
 import './App.css'
+import {SearchableList} from "./SearchableList.jsx";
 
+const list = [
+	{
+		id: "1",
+		title: "Арбуз"
+	},
+	{
+		id: "2",
+		title: "Огурец"
+	},
+	{
+		id: "3",
+		title: "Свёкла"
+	}
+]
 function App() {
 	return (
-		<>
-
-		</>
+			<SearchableList list={list}/>
 	)
 }
 
---
+++ module-3/src/SearchableList.jsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+
+export const SearchableList = ({list}) => {
+
+	const [searchString, setSearchString] = useState("");
+	const handleSeaarch = (event) => {
+		setSearchString(event.target.value);
+	}
+
+	const filteredList = list.filter((item) => item.title.toLowerCase().includes(searchString.toLowerCase()));
+
+
+	return (
+		<div>
+			<label htmlFor="">
+				<input type="text" value={searchString} onChange={handleSeaarch}/>
+			</label>
+			<ul>
+				{filteredList.map(({title, id}) => <li key={id}>{title}</li>)}
+			</ul>
+		</div>
+	);
+}
