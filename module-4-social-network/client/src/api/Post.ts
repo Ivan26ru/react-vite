de09f6b11da6e57065a2e75cab8208c152d46363
ivan <pisarenko@arnica.pro>
2024-04-12 18:58:52 +0500

    m4 4.4 9:10 RequestState

--- module-4-social-network/client/src/api/Post.ts
+++ module-4-social-network/client/src/api/Post.ts
@@ -11,3 +11,15 @@ export type Post = z.infer<typeof PostSchema>;
 export const PostList = z.array(PostSchema);
 
 export type PostList = z.infer<typeof PostList>;
+
+export const FetchPostListSchema = z.object({
+    list: PostList,
+})
+
+type FetchPostListResponse = z.infer<typeof FetchPostListSchema>;
+
+function fetchPostList(): Promise<FetchPostListResponse> {
+    return fetch("/api/posts")
+        .then((response) => response.json())
+        .then((data) => FetchPostListSchema.parse(data));
+}
--- module-4-social-network/client/src/api/User.ts
+++ module-4-social-network/client/src/api/User.ts
@@ -1,7 +1,45 @@
 import {z} from 'zod'
+import {PostList} from "./Post.ts";
+
 export const UserSchema = z.object({
     id: z.string(),
     username: z.string(),
 })
 
 export  type User = z.infer<typeof UserSchema>;
+
+export function fetchUser(id: string): Promise<User> {
+    return fetch(`/api/users/${id}`)
+        .then(response => response.json())
+        .then((data) => UserSchema.parse(data));
+}
+
+// состояния запросов
+
+interface IdleRequestState {
+    status: "Idle";
+}
+
+interface LoadingRequestState {
+    status: "Loading";
+}
+
+interface SuccessRequestState {
+    status: "Success";
+    data: PostList;
+}
+
+interface ErrorRequestState {
+    status: "Error";
+    data: unknown;
+}
+
+type RequestState =
+    | IdleRequestState
+    | LoadingRequestState
+    | SuccessRequestState
+    | ErrorRequestState;
+
+export function usePostList() {
+    const [] = useState({ status: "Idle" });
+}
