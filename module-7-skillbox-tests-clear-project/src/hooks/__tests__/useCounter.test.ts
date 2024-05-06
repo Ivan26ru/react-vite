f5bdde34816edd866d8dccd7220d31df52405b7b
Ivan <it-1ivan@ya.ru>
2024-05-06 21:39:59 +0500

    m7.6 test hook

---
+++ module-7-skillbox-tests-clear-project/src/hooks/__tests__/useCounter.test.ts
@@ -0,0 +1,23 @@
+import {act, renderHook} from "@testing-library/react";
+import {useCounter} from "../useCounter.ts";
+
+test('Увеличивает значение счетчика и устанавливает новое начальное значение', () => {
+    const {result, rerender} = renderHook(({initialValue}) => useCounter(initialValue), {
+        initialProps: {
+            initialValue: 0
+        }
+    })
+
+    act(() => {
+        result.current.increment()
+    })
+
+    expect(result.current.count).toBe(1);
+
+    rerender({initialValue: 10});
+
+    act(() => {
+        result.current.reset()
+    })
+    expect(result.current.count).toBe(10);
+});
