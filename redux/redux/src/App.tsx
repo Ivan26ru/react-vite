f13f7c84da47e4f79996bc7e760957b51da586b0
Ivan <it-1ivan@ya.ru>
2024-10-05 21:05:10 +0500

    add POST request for RTK

--- redux/redux/src/App.tsx
+++ redux/redux/src/App.tsx
@@ -4,28 +4,61 @@ import TodoList from "./components/TodoList.tsx";
 import InputField from "./components/InputField.tsx";
 import {addTodo} from "./store/todoSlice.ts";
 
-import {useGetTodosQuery} from "./store/todosApi";
+import {useGetTodosQuery, useAddProductMutation} from "./store/todosApi";
 
 import './App.css';
 
 function App() {
     const [text, setText] = useState('')
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
 
+    //
+    // const addTask = () => {
+    //     dispatch(addTodo({text}))
+    //     setText('')
+    // };
 
-    const addTask = () => {
-        dispatch(addTodo({text}))
-        setText('')
-    };
+    const [count, setCount] = useState(10);
+    const [newTodo, setNewTodo] = useState('');
+    const {data = [], isLoading} = useGetTodosQuery(count);
+    const [addTodo, {isError}] = useAddProductMutation();
 
-    const {data = [], isLoading} = useGetTodosQuery();
+    const handleAddTodo = async () => {
+        if (newTodo) {
+            await addTodo(
+                {
+                    title: newTodo,
+                    body:'test',
+                    userId:1}
+            ).unwrap();
+            console.log(newTodo)
+            setNewTodo('');
+        }
+    }
+
     if (isLoading) return <h1>Loading...</h1>
 
 
     return (
         <>
-            <InputField text={text} handleInput={setText} handleSubmit={addTask}/>
+            {/*<InputField */}
+            {/*    text={text} */}
+            {/*    handleInput={setText} */}
+            {/*    handleSubmit={addTask}*/}
+            {/*/>*/}
+            <input type="text"
+            value={newTodo}
+            onChange={(e)=>setNewTodo(e.target.value)}
+            />
+            <button onClick={handleAddTodo}>Add todo</button>
             <TodoList/>
+
+            <select name="" id="" value={count} onChange={(e) => setCount(e.target.value)}>
+                <option value="">all</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
             <div>
                 <ul>
                     {data.map(item => (
--- redux/redux/src/store/todosApi.ts
+++ redux/redux/src/store/todosApi.ts
@@ -2,12 +2,21 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const todosApi = createApi({
     reducerPath: 'todosApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com'}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://jsonplaceholder.typicode.com'
+    }),
     endpoints: (build) => ({
         getTodos: build.query({
-            query: () => 'todos',
+            query: (limit = '') => `todos?${limit && `_limit=${limit}`}`,
+        }),
+        addProduct: build.mutation({
+            query: (body) => ({
+                url: 'todos',
+                method: 'POST',
+                body
+            })
         })
     })
 });
 
-export const {useGetTodosQuery} = todosApi;
+export const {useGetTodosQuery, useAddProductMutation} = todosApi;
