e70079d2f872a881c3f75eb9b1653f2de8038366
Ivan <it-1ivan@ya.ru>
2024-10-06 00:32:59 +0500

    add autoupdate for add new Todo

--- redux/redux/src/store/todosApi.ts
+++ redux/redux/src/store/todosApi.ts
@@ -2,19 +2,28 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const todosApi = createApi({
     reducerPath: 'todosApi',
+    tagTypes: ['Todos'],
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://jsonplaceholder.typicode.com'
     }),
     endpoints: (build) => ({
         getTodos: build.query({
             query: (limit = '') => `todos?${limit && `_limit=${limit}`}`,
+            providesTags: (result) =>
+                result
+                    ? [
+                        ...result.map(({id}) => ({type: 'Todos' as const, id})),
+                        {type: 'Todos', id: 'LIST'},
+                    ]
+                    : [{type: 'Todos', id: 'LIST'}],
         }),
         addProduct: build.mutation({
             query: (body) => ({
                 url: 'todos',
                 method: 'POST',
                 body
-            })
+            }),
+            invalidatesTags: [{type: 'Todos', id: 'LIST'}]
         })
     })
 });
